9c1ce172e554d9923b3af5ed79fe3be3d0003968
WHITE DEVIL <white.devil.dev.141@gmail.com>
2026-06-06 06:31:15 +0600

    fix: use next.config.js instead of .mjs

---
+++ next.config.js
@@ -0,0 +1,2 @@
+const cp = process.env.NEXT_PUBLIC_BASE_PATH || '';
+export default { output: 'export', trailingSlash: true, basePath: cp, assetPrefix: cp || undefined, images: { unoptimized: true } };
